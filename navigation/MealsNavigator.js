1348cf6fc413f123a32b1334dcf2ede2d44c2fd1
Jaber Alsalamah <JaAlSaDev@gmail.com>
2021-07-26 10:08:39 +0300

    created bottom tab navigator

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,9 +1,15 @@
+import React from 'react'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+
 import CategoriesScreen from '../screens/CategoriesScreen'
 import CategoryMealsScreen from '../screens/CategoryMealsScreen'
 import MealDetailScreen from '../screens/MealDetailScreen'
+import FavoritesScreen from '../screens/FavoritesScreen'
+
 import { Platform } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
 import Colors from '../constants/Colors'
 
 const MealsNavigator = createStackNavigator({
@@ -26,10 +32,41 @@ const MealsNavigator = createStackNavigator({
         headerTintColor: Platform.OS === "android" ? 'white' : Colors.primaryColor,
         headerTitle: 'A Screen'
     },
-    mode:'modal',
+    mode: 'modal',
     // initialRouteName: 'MealDetail'
 })
 
 
+const MealsFavTabNavigator = createBottomTabNavigator({
+    Meals: {
+        screen: MealsNavigator,
+        navigationOptions: {
+            tabBarIcon: tabInfo => {
+                return <Ionicons
+                    name='ios-restaurant'
+                    size={25}
+                    color={tabInfo.tintColor}
+                />
+            }
+        }
+    },
+    Favorites: {
+        screen:FavoritesScreen,
+        navigationOptions: {
+            tabBarLabel:'Favorites!',
+            tabBarIcon: tabInfo => {
+                return <Ionicons
+                    name='ios-star'
+                    size={25}
+                    color={tabInfo.tintColor}
+                />
+            }
+        }
+    },
+}, {
+    tabBarOptions: {
+        activeTintColor: Colors.accent
+    }
+})
 
-export default createAppContainer(MealsNavigator)
+export default createAppContainer(MealsFavTabNavigator)
